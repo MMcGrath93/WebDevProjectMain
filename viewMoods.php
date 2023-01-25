aeec89f1751251bac43f34fdb7153bc7097f99eb
MMcGrath93 <91155688+MMcGrath93@users.noreply.github.com>
2023-01-25 17:33:59 +0000

    mood chart added

--- viewMoods.php
+++ viewMoods.php
@@ -1,13 +1,9 @@
 <?php
-
 include "dbconn.php";
 session_start();
 
-
-
 if (!isset($_SESSION['id'])) {
     header('Location: login.php');
-
 }
 $id = $_SESSION['id'];
 
@@ -23,84 +19,112 @@ $id = $_SESSION['id'];
     <title>My Moods</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
 </head>
 
 <body>
 
-    <?php
-    //import nav bar
-    include "./navbar.html";
-    ?>
+
+    <!--import nav bar-->
+    <?php include "./navbar.html"; ?>
 
     <br>
-    <!-- Create Form Action - POST -->
-    <form action="processLogin.php" method="post">
-
-        <!--Main Canvas-->
-        <div class="hero is-large">
-            <div class="container has-text-centered is-size-5">
-                <!--Content Box-->
-
-
-
-
-                <?php
-
-                $readSQL = "SELECT * FROM `moods` WHERE `user_id`='$id'";
-                $result = $conn->query($readSQL);
-                //Table Headers
-                echo '<table class="table is-bordered is-striped is-narrow is-hoverable">';
-                echo '<tr>
+    <div class="hero is-large">
+        <div class="container has-text-centered is-size-5">
+            <table class="table is-bordered is-striped is-narrow is-hoverable">
+                <tr>
                     <th class="has-background-white is-size-4">Mood</th>
                     <th class="has-background-white is-size-4">Notes</th>
                     <th class="has-background-white is-size-4">Date & Time Recorded</th>
                     <th class="has-background-white is-size-4"></th>
-                    </tr>';
+                </tr>
+                <?php
+                $readSQL = "SELECT * FROM `moods` WHERE `user_id`='$id'";
+                $result = $conn->query($readSQL);
 
                 while ($row = $result->fetch_assoc()) {
-
                     $mood = $row["context"];
                     $date = $row["datetime"];
                     $moodid = $row["id"];
-                    $moodchoice = $row["value"]
-                    ;
-                    //Add each row
-                
+                    $moodchoice = $row["value"];
                     echo "<tr>
-        <td>$moodchoice</td>
-<td>$mood</td>
-<td>$date</td>";
-                    echo "<td> <a href='editMood.php?mood_id=$moodid' class='button'>Edit</a> <a href='processDelete.php?moodid=$moodid'class='button is-danger'>Delete</a></td>
-</tr>";
-
+            <td>$moodchoice</td>
+            <td>$mood</td>
+            <td>$date</td>";
+                    echo "<td> <a href='editMood.php?mood_id=$moodid' class='button'>Edit</a> <a href='processDelete.php?moodid=$moodid' class='button is-danger'>Delete</a></td>
+                    </tr>";
                 }
-
-
-                echo '</table>';
-
-
                 ?>
-                <a href='export.php' class='button is-primary'>Export My Mood Log</a>
-            </div>
+            </table>
+            <a href='export.php' class='button is-primary'>Export My Mood Log</a>
         </div>
+    </div>
+    <div class="hero is-large">
+        <div class="hero-body is-justify-content-center is-align-items-center pt-6 pb-6">
+            <div class="columns is-flex is-flex-direction-column box">
+                <div class="column has-text-centered">
 
-        <div class="hero is-large">
-            <div class="hero-body is-justify-content-center is-align-items-center pt-6 pb-6">
-                <!--Visualisations-->
-                <div class="columns is-flex is-flex-direction-column box">
-                    <div class="column has-text-centered">
-                        <h1>Visualisations go here</h1>
-                    </div>
+                    <!--Visualisation-->
+                    <?php
+                    //get data to populate chart
+                    $moodLabels = array();
+                    $moodData = array();
+                    $readSQL = "SELECT value, COUNT(value) as count FROM moods WHERE user_id = '$id' GROUP BY value";
+                    $result = $conn->query($readSQL);
+                    while ($row = $result->fetch_assoc()) {
+                        $moodLabels[] = $row["value"];
+                        $moodData[] = $row["count"];
+                    }
+                    ?>
+                    <h3 class="title is-3">My Mood Chart</h3>
+                    <canvas id="myChart"></canvas>
                 </div>
-                <!--Visualisations End-->
             </div>
         </div>
-        <!--End of Main Canvas-->
-        <!--Footer-->
-        <div class="footer">
-            <?php include "./footer.html"; ?>
-        </div>
+    </div>
 
+    <div class="footer">
+        <?php include "./footer.html"; ?>
+    </div>
+
+    <!--Script to create mood Visualisation---->
+    <script>
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: <?php echo json_encode($moodLabels); ?>,
+                datasets: [{
+                    label: '# of Moods',
+                    data: <?php echo json_encode($moodData); ?>,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
 
 </body>
 
